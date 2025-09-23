5c5d86d3d6c8cceae6cc93e0c395b5d34db0cc78
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2025-09-22 22:04:23 -0400

    360 Upload

---
+++ data.js
@@ -0,0 +1,191 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-st-mary--st-michael-adoration-chapel-hollidaysburg-pa",
+      "name": "St. Mary & St. Michael Adoration Chapel Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.726657894150323,
+          "pitch": 0.6193631031269966,
+          "rotation": 0,
+          "target": "3-st-mary--st-michael-adoration-chapel-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-st-mary--st-michael-adoration-chapel-hollidaysburg-pa",
+      "name": "St. Mary & St. Michael Adoration Chapel Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.05804674511210983,
+        "pitch": 0.009222634618900827,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6634477157571013,
+          "pitch": 0.08204362929095232,
+          "rotation": 0,
+          "target": "2-st-mary--st-michael-adoration-chapel-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-st-mary--st-michael-adoration-chapel-hollidaysburg-pa",
+      "name": "St. Mary & St. Michael Adoration Chapel Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7714799748237535,
+          "pitch": 0.07986467565068622,
+          "rotation": 0,
+          "target": "1-st-mary--st-michael-adoration-chapel-hollidaysburg-pa"
+        },
+        {
+          "yaw": 1.1411316336307209e-11,
+          "pitch": 0.08418256582494799,
+          "rotation": 0,
+          "target": "3-st-mary--st-michael-adoration-chapel-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-st-mary--st-michael-adoration-chapel-hollidaysburg-pa",
+      "name": "St. Mary & St. Michael Adoration Chapel Hollidaysburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.639005294549797,
+          "pitch": 0.11113260880649634,
+          "rotation": 0,
+          "target": "2-st-mary--st-michael-adoration-chapel-hollidaysburg-pa"
+        },
+        {
+          "yaw": -2.7844846019768053,
+          "pitch": 0.38820001675560967,
+          "rotation": 0,
+          "target": "0-st-mary--st-michael-adoration-chapel-hollidaysburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
